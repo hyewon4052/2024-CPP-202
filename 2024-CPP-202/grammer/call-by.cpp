6009b895920529e3841ea02778499cf22a85b42b
hyewon4052 <s2311@e-mirim.hs.kr>
2024-03-26 15:33:51 +0900

    call by value

---
+++ 2024-CPP-202/grammer/call-by.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+// 이름공간. std::를 안해도 된다.
+using namespace std;
+
+void call_by_value(int val) {
+
+}
+
+int main(void) {
+	int num = 4;
+	call_by_value(num);
+	cout << num;
+	return 0;
+}
