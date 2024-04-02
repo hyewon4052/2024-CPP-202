5da865723dc5cee0045c6960b5ab986af9051d8b
hyewon4052 <s2311@e-mirim.hs.kr>
2024-04-02 16:04:56 +0900

    함수 오버로딩 진행 후

--- 2024-CPP-202/grammer/02-02 함수 오버로딩.cpp
+++ 2024-CPP-202/grammer/02-02 함수 오버로딩.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int int_mul (int a, int b) {
+int mul (int a, int b) {
 	return a * b;
 }
 
-float int_mul(float a, float b) {
+float mul(float a, float b) {
 	return a * b;
 }
 
 int main(void) {
-	cout << int_mul(2, 2) << endl;
-	cout << int_mul(2.2f, 2.2f) << endl;
+	cout << mul(2, 2) << endl;
+	cout << mul(2.2f, 2.2f) << endl;
 	return 0;
 }
