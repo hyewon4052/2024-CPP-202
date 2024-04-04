1fd45d91d7cccaa7c15db3e315984e5caa5a4943
hyewon4052 <s2311@e-mirim.hs.kr>
2024-04-04 11:18:31 +0900

    string 대입1 (C언어 방식)

---
+++ 2024-CPP-202/grammer/02-03 String대입(복사).cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main(void) {
+	char str[30] = "develop hungry";
+	char str2[30];
+	// str에 있는 내용을 str2에 복사
+	
+	int i = 0;
+	for (; str[i] != '\0'; i++) {
+		str2[i] = str[i];
+	}
+	str2[i] = str[i];
+
+	printf("%s", str2);
+	
+}
