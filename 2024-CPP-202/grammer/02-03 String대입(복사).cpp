05d962fd568cd32dba3197ca0cca55991dca396a
hyewon4052 <s2311@e-mirim.hs.kr>
2024-04-04 11:47:29 +0900

    string 대입3(C++ 방식)

--- 2024-CPP-202/grammer/02-03 String대입(복사).cpp
+++ 2024-CPP-202/grammer/02-03 String대입(복사).cpp
@@ -1,13 +1,12 @@
-﻿#include <stdio.h>
-#include <string.h>
+﻿#include <iostream>
+#include <string>
 
 int main(void) {
-	char str[30] = "develop hungry";
-	char str2[30];
-	// str에 있는 내용을 str2에 복사
-	
-	strcpy(str2, str);		// 보안에 취약한 함수 
+	std::string str = "develop hungry";
+	std::string str2;
 
-	printf("%s", str2);
-	
+	// str에 있는 내용을 str2에 복사
+	str2 = str;
+
+	std::cout << str2;
 }
