d907982179d4b21308f8bee4ffab99e095c9df99
hyewon4052 <s2311@e-mirim.hs.kr>
2024-04-04 11:17:56 +0900

    init commit

--- 2024-CPP-202/grammer/배열과 포인터 그리고 함수.cpp
+++ 2024-CPP-202/grammer/배열과 포인터 그리고 함수.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+﻿#include <iostream> 
 
 // 배열의 모든 요소를 0으로 바꿔준다.
 void erase(int* ptr,int length) {	
@@ -6,7 +6,6 @@ void erase(int* ptr,int length) {
 	for (int i = 0; i <= length; i++) {
 		ptr[i] = 0;
 	}
-
 }
 
 int main(void) {
