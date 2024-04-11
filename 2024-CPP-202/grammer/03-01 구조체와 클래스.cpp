d06a362279f9bfb8f0716b685c8c7ca8cd5bf1bb
hyewon4052 <s2311@e-mirim.hs.kr>
2024-04-11 11:34:45 +0900

    03-01 포인터연산 간략하게

--- 2024-CPP-202/grammer/03-01 구조체와 클래스.cpp
+++ 2024-CPP-202/grammer/03-01 구조체와 클래스.cpp
@@ -3,38 +3,48 @@
 using namespace std;
 
 class Student {
-public: 
-	// 생성자 : 객체가 생성될 때 호출되는 함수
-	// 생성자는 반환형이 없다.
-	Student() {
-		hakbun_ = 2222;
-		name_ = "swag";
-	}
-	Student(int hakbun, string name) {
-		hakbun_ = hakbun;
-		name_ = name;
-	}
-	// 클래스의 멤버를 출력
-	void show(void) {
-		cout << hakbun_ << endl;
-		cout << name_ << endl;
-	}
+public:
+    // 생성자 : 객체가 생성될 때 호출되는 함수
+    // 생성자는 반환형이 없다.
+    Student()
+    {
+        hakbun_ = 2211;
+        name_ = "swag";
+    }
 
-private :
-	int hakbun_;	
-	string name_;
+    Student(int hakbun, string name)
+    {
+        hakbun_ = hakbun;
+        name_ = name;
+    }
+
+    // 클래스의 멤버를 출력
+    void show(void)
+    {
+        cout << hakbun_ << " ";
+        cout << name_ << endl;
+    }
+
+private:
+    int hakbun_;
+    string name_;
+
+
 };
 
-int main(void) 
-{	
-	// 동적할당 : 메모리는 heap에서 할당. 실행 (런타임) 시 메모리크기가 정해짐
-	Student* jisu = Student(2202,"나지수");
+int main(void)
+{
+    // 동적할당 : 메모리는 heap에서 할당. 실행(런타임) 시 메모리 크기가 정해짐
+    Student* jisu = new Student(2202, "나지수");
 
-	// 정적할당 : 메모리는 stack에서 할당
-	Student swag = Student();
+    // 정적할당 : 메모리는 stack에서 할당. 컴파일 시 메모리 크기가 정해짐
+    Student swag = Student();
 
-	(*jisu).show();
-	swag.show();
-	return 0;
+    jisu->show();      // jisu->show();
+    swag.show();
+
+    // 동적할당은 무조건 해제 
+    delete jisu;
+
+    return 0;
 }
-
