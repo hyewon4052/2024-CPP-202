70abbb13a8b38834f7244aded02a5b212414b090
hyewon4052 <s2311@e-mirim.hs.kr>
2024-04-16 16:02:23 +0900

    03-02 생성자에서 동적할당을 한 메모리 공간을 지울 수 없다.

---
+++ 2024-CPP-202/grammer/03-02 생성자와 소멸자.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Student {
+public:
+
+    // ex) name: "domitory"
+    Student(int hakbun, char* name) : hakbun_(hakbun)
+    {
+
+        int length = strlen(name);
+
+        // '/0'가 들어가기 위해 공간 하나 더 추가
+        name_ = new char[length+1];
+        strcpy(name_, name);
+    }
+
+    // 클래스의 멤버를 출력
+    void show(void)
+    {
+        cout << hakbun_ << " ";
+        cout << name_ << endl;
+    }
+
+private:
+    int hakbun_;
+    char* name_;
+
+};
+
+int main(void)
+{
+    Student stu = Student(2213, (char*)"조수빈");
+    stu.show();
+
+    return 0;
+}
