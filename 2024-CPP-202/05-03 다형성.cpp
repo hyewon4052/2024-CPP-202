21f169a5ba07b9fb9114afb2493df8db34549ec1
hyewon4052 <s2311@e-mirim.hs.kr>
2024-08-22 11:22:10 +0900

    다형성 (동적 바인딩)

--- 2024-CPP-202/05-03 다형성.cpp
+++ 2024-CPP-202/05-03 다형성.cpp
@@ -8,21 +8,14 @@ class Animal {
 public : 
 	Animal(string name, unsigned int age, int leg_num) 
 		: name_(name), age_(age), leg_num_(leg_num) {
-		cout << "이름 : " << name << endl;
-		cout << "나이 : " << age << endl;
-		cout << "다리갯수 : " << leg_num << endl;
+		cout << "이름 : " << name_ << endl;
+		cout << "나이 : " << age_ << endl;
+		cout << "다리갯수 : " << leg_num_ << endl;
 	}
 
-	void walk(void) {
-		cout << "걷다" << endl;
-	}
-	void bark(void) {
-		cout << "짖다" << endl;
-	}
-	void eat(void) {
-		cout << "먹다" << endl;
-
-	}
+	virtual void walk(void) { cout << "걷다" << endl; }
+	virtual void bark(void) { cout << "짖다" << endl; }
+	virtual void eat(void) { cout << "먹다" << endl; }
 
 private :
 	string name_;
@@ -38,15 +31,9 @@ public:
 		cout << "충성도" << endl;
 	}
 	// C++은 디폴트가 정적바인딩이기 때문에 가상함수로 오버라이딩 해야 한다.
-	void bark() {
-		cout << "울프울프" << endl;
-	}
-	void eat() {
-		cout << "왕~왕~" << endl;
-	}
-	void walk() {
-		cout << "촵촵촵촵" << endl;
-	}
+	void bark() { cout << "울프울프" << endl; }
+	void eat() { cout << "왕~왕~" << endl; }
+	void walk() { cout << "촵촵촵촵" << endl; }
 private:
 	int loyalty_;
 };
@@ -59,8 +46,8 @@ void main(void) {
 	delete animal;
 
 	animal = new Dog("마루", 5, 2, 100);
-	delete animal;
 	animal->bark();
 	animal->eat();
 	animal->walk();
+	delete animal;
 }
