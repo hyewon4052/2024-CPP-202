1ef9584f35a12d017502a49151bba90f832c4a6e
hyewon4052 <s2311@e-mirim.hs.kr>
2024-09-03 13:57:15 +0900

    다형성 적용 전

--- 2024-CPP-202/grammer/다형성 예제.cpp
+++ 2024-CPP-202/grammer/다형성 예제.cpp
@@ -9,6 +9,13 @@ public :
 		civill_(civill), force_(force),name_(name),territory_(territory) {
 
 	}
+
+	void show() {
+		cout << "국민수 : " << civill_ << endl;
+		cout << "군력 : " << force_ << endl;
+		cout << "이름 : " << name_ << endl;
+		cout << "면적 : " << territory_ << endl;
+	}
 private:
 	int civill_;	// 국민 수 
 	int force_;	// 군력
@@ -22,6 +29,10 @@ public:
 		: Food(civill, force, name, territory), garic_(garic), pepper_(pepper) {
 
 	}
+	void show() {
+		cout << "마늘 : " << garic_ << endl;
+		cout << "고춧가루 : " << pepper_ << endl;
+	}
 private:
 	int garic_;
 	int pepper_;
@@ -34,6 +45,10 @@ public:
 		: Food(civill, force, name, territory), gelatin_(gelatin), sugar_(sugar) {
 
 	}
+	void show() {
+		cout << "젤라틴 : " << gelatin_ << endl;
+		cout << "설탕 : " << sugar_ << endl;
+	}
 private:
 	int gelatin_;
 	int sugar_;
@@ -41,12 +56,30 @@ private:
 
 class Cheese : public Food {
 public:
-	Cheese(int civill, int force, string name, int territory, int milk, int Rennet)
-		: Food(civill, force, name, territory), milk_(milk), Rennet_(Rennet) {
+	Cheese(int civill, int force, string name, int territory, int milk, int rennet)
+		: Food(civill, force, name, territory), milk_(milk), rennet_(rennet) {
 
+	}
+	void show() {
+		cout << "우유 : " << milk_ << endl;
+		cout << "응고제 : " << rennet_ << endl;
 	}
 private:
 	int milk_;
-	int Rennet_;	// 응고제
+	int rennet_;	// 응고제
 
 };
+
+void main(void) {
+	Food* player = new Kimchi(15, 95, "갓김치", 86, 100, 100);
+	Food* friends = new Cheese(100, 20, "짜계치",100,20,100);
+
+	player->show();
+	cout << endl << endl;
+	friends->show();
+
+	delete friends;
+	delete player;
+
+
+}
