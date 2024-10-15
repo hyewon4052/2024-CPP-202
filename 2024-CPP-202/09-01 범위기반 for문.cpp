05eca9310a05c4a34683a65439d2b72689956235
hyewon4052 <s2311@e-mirim.hs.kr>
2024-10-15 14:29:29 +0900

    09-01 범위기반 for문

--- 2024-CPP-202/09-01 범위기반 for문.cpp
+++ 2024-CPP-202/09-01 범위기반 for문.cpp
@@ -7,10 +7,10 @@ using namespace std;
 void main(void) {
 
 	vector <int> v = {5,6,13,84,65};
-	for (int value : v) {
+	for (int value : v) {	// value는 지역 변수라서 값이 바뀌지 않음
 		value *= 10;
 	}
-	for (int value : v) {
+	for (int value : v) {	
 		cout << value << endl;
 	}
 
