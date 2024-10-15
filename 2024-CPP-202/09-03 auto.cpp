21b9ded83ac0594da6bbac244bb48cd045806d3b
hyewon4052 <s2311@e-mirim.hs.kr>
2024-10-15 14:55:10 +0900

    09-03 auto

---
+++ 2024-CPP-202/09-03 auto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void main(void) {
+	vector <string> v = { "롯데관광","동화면세점","제주드림타운","마이데일리" };
+	for (auto str : v) {
+		cout << str << endl;
+	}
+	auto a = 30;
+
+}
