946773026a6ebe76de1caff2b61a51dba6257a7e
hyewon4052 <s2311@e-mirim.hs.kr>
2024-10-17 12:01:27 +0900

    [sG} 상수처리 방법1

--- shootingG/main.cpp
+++ shootingG/main.cpp
@@ -3,8 +3,11 @@
 using namespace sf;
 using namespace std;
 
+#define WINDOW_WIDTH  1200
+#define WINDOW_HEIGHT 700
+
 void main(void) {
-	RenderWindow window(VideoMode(1000, 800), "shootingG");
+	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "shootingG");
 	while (window.isOpen()) {
 		Event event;
 		while (window.pollEvent(event)) {	
